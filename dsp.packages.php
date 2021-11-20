3feda57f12fe77b0df1b72d0dde9088b15733d68
Longinus <jesuscaesar@outlook.com>
2021-11-20 13:27:37 +0000

    Latest update

--- dsp.packages.php
+++ dsp.packages.php
@@ -1,7 +1,7 @@
 <?php
 foreach ($pkgList as $key=>$value) {
     $packageID = basename($value, '.pkg');
-    $fileTitle = 'Remove '.$packageID;
+    $fileTitle = $packageID;
     $fileLink = "get('d', '".$packageID."', 'from', 'here');";
 ?>
 <input type="button" onclick="<?=$fileLink;?>" value="<?=$fileTitle;?>">
