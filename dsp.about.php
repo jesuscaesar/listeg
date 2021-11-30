2151b2ec1295267ade2ef5154c51e42edd866eed
Longinus <jesuscaesar@outlook.com>
2021-11-30 08:15:20 +0000

    Latest update

--- dsp.about.php
+++ dsp.about.php
@@ -1,6 +1,8 @@
 <?php
 $systemInfo = file_get_contents('system.info');
 $systemData = explode('=||=', $systemInfo);
+foreach ($systemData as $key=>$value) {
+    $valueExp = explode('||', $value);
 ?>
-<p><label><b>Name:</b> <?=$systemData[0];?></label></p>
-<p><label><b>Version:</b> <?=$systemData[1];?></label></p>
+<label><b><?=$valueExp[0];?></b> <?=$valueExp[1];?></label><br>
+<?php } ?>
