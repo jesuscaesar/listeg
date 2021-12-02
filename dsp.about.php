256c3959a0c5e58d0bd91e5a4e5c2b318c481db4
Longinus <jesuscaesar@outlook.com>
2021-12-02 10:46:26 +0000

    Latest update

--- dsp.about.php
+++
@@ -1,8 +0,0 @@
-<?php
-$systemInfo = file_get_contents('system.info');
-$systemData = explode('=||=', $systemInfo);
-foreach ($systemData as $key=>$value) {
-    $valueExp = explode('||', $value);
-?>
-<label><b><?=$valueExp[0];?></b> <?=$valueExp[1];?></label><br>
-<?php } ?>
